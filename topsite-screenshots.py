c311065a56d3b317b7388d0fd3cd96eb458a404c
Aaron Train <aaron.train@gmail.com>
2012-01-22 17:37:47 -0500

    refactoring and cleanup

--- topsite-screenshots.py
+++ topsite-screenshots.py
@@ -2,14 +2,31 @@
 
 __author__ = 'aaron.train@gmail.com'
 
-import sys, getopt, json, os, time
+import sys
+import optparse
+import json
+import os
+import time
+
+_browser_intents = {'android' : 'com.google.android.browser/com.android.browser.BrowserActivity',
+					'fennec-native' : 'org.mozilla.fennec/.App',
+              		'opera' : 'com.opera.browser/com.opera.Opera'}
+
+class StartupOptions(optparse.OptionParser):
+	def __init__(self, configFile=None, **kwargs):
+		optparse.OptionParser.__init__(self, **kwargs)
+		defaults = {}
+
+		self.add_option("--sites", action="store", dest="sites", type="string",
+						help="Provide a JSON list of top sites")
+		defaults["sites"] = None
 
 class topsite_screenshots:
-	def __init__(self, sites, browsers):
-		self._list_of_sites = self._read_json(sites)
-		self._list_of_browsers = self._read_json(browsers)
+	def __init__(self, options):
+		self.sites = options.sites
 
 	def Run(self):
+		self.sites = self._read_json(self.sites)
 		self._topsite_screenshots()
 
 	def _read_json(self, _jsonfile):
@@ -18,17 +35,17 @@ class topsite_screenshots:
 		return self.json_data
 
 	def _topsite_screenshots(self):
-		for self.n, self.x in enumerate(self._list_of_browsers):
-			for self.o, self.y in enumerate(self._list_of_sites):
-				self._startBrowserIntent(self.x, self.y)
-				time.sleep(12) #Hack: Need to figure out how to wait for page load
-				self._createScreenshot("%s-%s" % (self.o, self.n))
-
-	def _startBrowserIntent(self, _browser_intent, _site):
-		os.system("adb shell am start -a android.intent.action.VIEW -n %s %s%s" % (_browser_intent, "http://", _site))
-
-	def _createScreenshot(index):
 		self._createDir()
+		for index, intent in enumerate(_browser_intents.itervalues()):
+			for site in self.sites:
+				self._startBrowserIntent(intent, site)
+				time.sleep(12) #Hack: Need to figure out wait for page load
+				self._createScreenshot("%s-%s" % (self.sites.index(site), index))
+
+	def _startBrowserIntent(self, intent, site):
+		os.system("adb shell am start -a android.intent.action.VIEW -n %s %s" % (intent, site))
+
+	def _createScreenshot(self, index):
 		os.system("adb pull /dev/graphics/fb0 fb0")
 		os.system("dd bs=1920 count=800 if=fb0 of=fb0b")
 		os.system("ffmpeg -vframes 1 -vcodec rawvideo -f rawvideo -pix_fmt rgb32 -s 480x800 -i fb0b -f image2 -vcodec png %s.png" % (index))
@@ -44,26 +61,13 @@ class topsite_screenshots:
 			os.chdir(timestamp)
 
 def main():
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "", ["sites=", "browsers="])
-	except getopt.error, msg:
-		print 'python topsite-screenshots.py --sites [sites (JSON)] --browsers [browser intents (JSON)]'
-		sys.exit(2)
-	
-	sites = ""
-	browsers = ""
+	parser = StartupOptions()
+	options, args = parser.parse_args()
 
-	for o, a in opts:
-		if o == "--sites":
-			sites = a
-		if o == "--browsers":
-			browsers = a
+	if not options:
+		raise Exception('Options', 'Invalid options passed to topsite-screenshots')
 
-	if sites == "" or browsers == "":
-		print 'python topsite-screenshots.py --sites [sites (JSON)] --browsers [browser intents (JSON)]'
-		sys.exit(2)
-
-	n = topsite_screenshots(sites, browsers)
+	n = topsite_screenshots(options)
 	n.Run()
 
 if __name__ == "__main__":
